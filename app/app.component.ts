b1a806da1107d838bfc0c63a29e40b68d112734a
Tarun Kumar <tarun.kumar@algoworks.com>
2016-11-01 15:30:02 +0530

    First draft

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<h1>Hello World!!</h1>
+    <strong>This is my first Angular 2 application</strong>`
+})
+export class AppComponent { }
